0f11f34f02c72f9b6031a42d013f0ed694bb6cbc
Domen Tabernik <domen.tabernik@fri.uni-lj.si>
2021-06-30 16:48:50 +0200

    Updated docker folders and added build script

---
+++ docker/build-dockers.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker build detectron -t detectron-traffic-signs:ubuntu16.04-cuda10.0-cudnn7  \
+                        --build-arg CUDA_VERSION=10.0-cudnn7 \
+                        --build-arg OS_VERSION=ubuntu16.04
+
+docker build vicos-demo -t tsr-vicos-demo:ubuntu16.04-cuda10.0-cudnn7 \
+                        --build-arg OS_VERSION=ubuntu:16.04 \
+                        --build-arg DETECTRON_IMAGE_RUNTIME=detectron-traffic-signs:ubuntu16.04-cuda10.0-cudnn7
