6b3111f79727add35c6ee0ff7958924415eeb018
Domen Tabernik <domen.tabernik@fri.uni-lj.si>
2023-06-28 18:32:34 +0200

    Moved vicos-cube docker building to seperate repo

--- docker/build-dockers-cuda10.sh
+++ docker/build-dockers-cuda10.sh
@@ -3,13 +3,3 @@
 docker build detectron -t detectron-traffic-signs:ubuntu16.04-cuda10.0-cudnn7  \
                         --build-arg CUDA_VERSION=10.0-cudnn7 \
                         --build-arg UBUNTU_VERSION=16.04
-
-docker build vicos-demo -t tsr-vicos-demo:ubuntu16.04-cuda10.0-cudnn7-resnet50 \
-                        --build-arg UBUNTU_VERSION=16.04 \
-                        --build-arg DETECTRON_IMAGE_RUNTIME=detectron-traffic-signs:ubuntu16.04-cuda10.0-cudnn7 \
-                        --build-arg BACKBONE=resnet50
-
-docker build vicos-demo -t tsr-vicos-demo:ubuntu16.04-cuda10.0-cudnn7-resnet101 \
-                        --build-arg UBUNTU_VERSION=16.04 \
-                        --build-arg DETECTRON_IMAGE_RUNTIME=detectron-traffic-signs:ubuntu16.04-cuda10.0-cudnn7 \
-                        --build-arg BACKBONE=resnet101
--- docker/build-dockers-cuda11.sh
+++ docker/build-dockers-cuda11.sh
@@ -4,13 +4,3 @@ set -e
 docker build detectron_cuda11 -t detectron-traffic-signs:ubuntu16.04-cuda11.1-cudnn8  \
                         --build-arg CUDA_VERSION=11.1-cudnn8 \
                         --build-arg UBUNTU_VERSION=16.04
-
-docker build vicos-demo -t tsr-vicos-demo:ubuntu16.04-cuda11.1-cudnn8-resnet50 \
-                        --build-arg UBUNTU_VERSION=16.04 \
-                        --build-arg DETECTRON_IMAGE_RUNTIME=detectron-traffic-signs:ubuntu16.04-cuda11.1-cudnn8 \
-                        --build-arg BACKBONE=resnet50
-
-# docker build vicos-demo -t tsr-vicos-demo:ubuntu16.04-cuda11.1-cudnn8-resnet101 \
-#                         --build-arg UBUNTU_VERSION=16.04 \
-#                         --build-arg DETECTRON_IMAGE_RUNTIME=detectron-traffic-signs:ubuntu16.04-cuda11.1-cudnn8 \
-#                         --build-arg BACKBONE=resnet101
